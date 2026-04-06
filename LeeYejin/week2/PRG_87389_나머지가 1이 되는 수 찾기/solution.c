d1e8adf3918baa68f6c7d862230c846261a5a9da
yj9120 <113438709+yj9120@users.noreply.github.com>
2026-04-06 20:16:36 +0900

    Create solution.c

---
+++ LeeYejin/week2/PRG_87389_나머지가 1이 되는 수 찾기/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;    
+    for(int x = n; x > 0; x--){
+        if(n % x == 1){
+           answer = x; 
+        }
+    }
+    return answer;
+}
